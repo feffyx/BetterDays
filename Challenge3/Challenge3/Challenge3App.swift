143ffe7089167ddba6aa65f364ed4c9e347898e9
feffyx <ziacofederica@gmail.com>
2024-12-09 17:35:34 +0100

    initial commit

---
+++ Challenge3/Challenge3/Challenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge3App.swift
+//  Challenge3
+//
+//  Created by Federica Ziaco on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Challenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
